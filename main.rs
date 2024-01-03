3f0554d4aadadcb805358153b797ab6fcf76fddb
JakkobMath <115361418+JakkobMath@users.noreply.github.com>
2024-01-02 22:00:00 -0500

    Update main.rs
    Removed a compiler error from the speculative evals module

--- main.rs
+++ main.rs
@@ -2479,12 +2479,13 @@ pub(crate) mod chess {
 
 pub(crate) mod search {
     pub(crate) mod eval_abstracts {
+        use std::ops::Neg;
         pub(crate) trait NegamaxCompatible: Ord + Neg {}
         pub(crate) trait ABCompatible: Ord + Neg {
             type WindowParams: Default;
             type Window: Neg;
-            fn window_about(&self, params: WindowParams) -> Window;
-            fn in_window(&self, window: Window) -> bool;
+            fn window_about(&self, params: Self::WindowParams) -> Self::Window;
+            fn in_window(&self, window: Self::Window) -> bool;
         }
     }
 }
