99ead227db0c1a16991c29233561c93dafe9a41f
JakkobMath <115361418+JakkobMath@users.noreply.github.com>
2024-01-03 13:58:29 -0500

    Create implementations.rs
    Manual merger

---
+++ chess/implementations.rs
@@ -0,0 +1,9 @@
+
+// This is where actual implementations of the traits in the above module go. 
+// The types here allow you to actually store chess-related data and call 
+// chess-related functions, and later impls_version submodules are expected 
+// to become progressively better at this. 
+
+use super::abstracts::{helper_traits::*, helper_types::*};
+
+pub(crate) mod impls_v0;
