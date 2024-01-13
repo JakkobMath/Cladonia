f28760bee87bd555b1a0bd72739d27ede64adde0
JakkobMath <115361418+JakkobMath@users.noreply.github.com>
2024-01-12 23:15:08 -0500

    Starting new filler eval (PeSTO)
    Borrowing PeSTO's piece square tables briefly so I can iterate on the search more quickly. Will return to some other evaluation before producing data that might end up in the final version.

--- chess/implementations/impls_vzero.rs
+++ chess/implementations/impls_vzero.rs
@@ -908,6 +908,61 @@ fn naive_evaluation_stm(position: &UnwrappedFen) -> i16 {
     sum
 }
 
+// :crabgrab: took this from https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function on 
+// advice of the chess programming discord. Will make my own later, only using these values as 
+// a stand-in so I can properly iterate on the search design. I plan to produce no training data 
+// from these tables. 
+
+// Indexing differently <=> swapping black and white, makes this easier to copy. 
+const PESTO_MG_PAWN_B: [i16; 64] = [
+      0,   0,   0,   0,   0,   0,  0,   0, 
+     98, 134,  61,  95,  68, 126, 34, -11, 
+     -6,   7,  26,  31,  65,  56, 25, -20, 
+    -14,  13,   6,  21,  23,  12, 17, -23, 
+    -27,  -2,  -5,  12,  17,   6, 10, -25, 
+    -26,  -4,  -4, -10,   3,   3, 33, -12, 
+    -35,  -1, -20, -23, -15,  24, 38, -22, 
+      0,   0,   0,   0,   0,   0,  0,   0, 
+];
+
+const PESTO_EG_PAWN_B: [i16; 64] = [
+      0,   0,   0,   0,   0,   0,   0,   0, 
+    178, 173, 158, 134, 147, 132, 165, 187, 
+     94, 100,  85,  67,  56,  53,  82,  84, 
+     32,  24,  13,   5,  -2,   4,  17,  17, 
+     13,   9,  -3,  -7,  -7,  -8,   3,  -1, 
+      4,   7,  -6,   1,   0,  -5,  -1,  -8, 
+     13,   8,   8,  10,  13,   0,   2,  -7, 
+      0,   0,   0,   0,   0,   0,   0,   0, 
+];
+
+const PESTO_MG_KNIGHT_B: [i16; 64] = [
+    -167, -89, -34, -49,  61, -97, -15, -107, 
+     -73, -41,  72,  36,  23,  62,   7,  -17, 
+     -47,  60,  37,  65,  84, 129,  73,   44, 
+      -9,  17,  19,  53,  37,  69,  18,   22, 
+     -13,   4,  16,  13,  28,  19,  21,   -8, 
+     -23,  -9,  12,  10,  19,  17,  25,  -16, 
+     -29, -53, -12,  -3,  -1,  18, -14,  -19, 
+    -105, -21, -58, -33, -17, -28, -19,  -23, 
+];
+
+const PESTO_EG_KNIGHT_B: [i16; 64] = [
+    -58, -38, -13, -28, -31, -27, -63, -99,
+    -25,  -8, -25,  -2,  -9, -25, -24, -52,
+    -24, -20,  10,   9,  -1,  -9, -19, -41,
+    -17,   3,  22,  22,  22,  11,   8, -18,
+    -18,  -6,  16,  25,  16,  17,   4, -18,
+    -23,  -3,  -1,  15,  10,  -3, -20, -22,
+    -42, -20, -10,  -5,  -2, -20, -23, -44,
+    -29, -51, -23, -15, -22, -18, -50, -64,
+];
+
+fn pesto_evalutation_stm(position: &UnwrappedFen) -> i16 {
+    todo!();
+    0
+}
+
 // Higher score -> put move earlier. 
 // Not doing anything fancy yet- basically just raw naive MVV-LVA with some special cases. 
 // Might add in a forward movement bonus later or something. ... maybe just adding it now. 
