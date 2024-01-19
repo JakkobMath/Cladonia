4839deee1fa541a1d9dd869a9b8ac770f265c985
JakkobMath <115361418+JakkobMath@users.noreply.github.com>
2024-01-18 21:07:16 -0500

    Prepare to make bitboards
    New file and part of an outline

---
+++ chess/implementations/impls_v_one/board_rep.rs
@@ -0,0 +1,21 @@
+
+// Time to write bitboard code I guess
+
+// Colored for ___
+
+// Piecey for ___
+
+// Contentsy for ___
+
+// Ranked for ___
+
+// Filed for ___
+
+// Squarey for ___
+
+// HasBoard for ___
+// Note to self: maybe there's an indexed_by trait I could use to replace "canonical array"?
+
+// PlyCounting for ___
+
+// MoveCounting for ___
